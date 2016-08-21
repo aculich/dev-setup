47853ee0d340299ba806285a72dad6b72bdbd1f5
me <me@mes-MacBook.local>
2016-08-21 16:15:11 -0700

    user-level scripts to run

---
+++ user.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+./caps-to-control.sh
+./osx/accessories.sh
+./osx/activity-monitor.sh
+./osx/appstore.sh
+./osx/dock.sh
+./osx/finder.sh
+./osx/general.sh
+./osx/google-chrome.sh
+./osx/screen.sh
+./osx/terms.sh
+./osx/utils.sh
