5fd8b8d94eb999e23722a0e9b58ecaf0a7238600
Aaron Culich <aculich@gmail.com>
2016-08-23 18:58:52 -0700

    Ignore missing keyboard.modifiermapping domain/default pair
    The domain/default pair of (kCFPreferencesAnyApplication, com.apple.keyboard.modifiermapping.4176-1031 1452-629-0) does not exist

--- caps-to-control.sh
+++ caps-to-control.sh
@@ -16,7 +16,7 @@
 # list of keyboards plugged in to this computer
 keyboard_ids=$(ioreg -n IOHIDKeyboard -r | grep -E 'VendorID"|ProductID' | awk '{ print $4 }' | paste -s -d'-\n' -)
 # check if the keyboards are already remapped
-echo $keyboard_ids | xargs -I{} sh -c 'defaults -currentHost read -g "com.apple.keyboard.modifiermapping.{}-0" | grep "Dst = 2" > /dev/null'
+echo $keyboard_ids | xargs -I{} sh -c 'defaults -currentHost read -g "com.apple.keyboard.modifiermapping.{}-0" 2>/dev/null | grep "Dst = 2" > /dev/null'
 if [[ $? -ne 0 ]]; then
   # remap the keyboards
   echo $keyboard_ids | xargs -I{} defaults -currentHost write -g "com.apple.keyboard.modifiermapping.{}-0" -array "<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>"
