77e4285c2584c7cb36d2fb8a64fc3c9882f3d6a3
Aaron Culich <aculich@gmail.com>
2018-08-26 11:54:47 -0700

    use brew version of tccutil

--- accessibility.sh
+++ accessibility.sh
@@ -6,6 +6,6 @@ brew list tccutil >/dev/null || brew install tccutil
 
 list="com.amethyst.Amethyst com.apple.Terminal com.googlecode.iterm2 org.pqrs.Karabiner-AXNotifier com.binaryage.TotalSpaces2 com.divisiblebyzero.Spectacle"
 for i in $list; do
-  sudo tccutil --insert $i
-  sudo tccutil --enable $i
+  sudo /usr/local/bin/tccutil --insert $i
+  sudo /usr/local/bin/tccutil --enable $i
 done
