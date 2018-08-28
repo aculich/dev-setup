ac673a0c37449f058bcc93c6ee25a58787bd2403
Aaron Culich <aculich@gmail.com>
2018-08-27 19:32:53 -0700

    save user-defined hotkeys from NSUserKeyEquivalents

---
+++ save-hotkeys.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# save-hotkeys.sh
+# https://superuser.com/questions/670584/how-can-i-migrate-all-keyboard-shortcuts-from-one-mac-to-another#
+# https://www.raizlabs.com/dev/2015/02/how-to-script-os-x-keyboard-shortcuts/
+
+# Command:  @
+# Control:  ^
+# Option:   ~
+# Shift:    $
+# Tab: \U21e5 (Unicode code point for ⇥ character)
+
+COMMAND_KEY_SYMBOL="@"
+CONTROL_KEY_SYMBOL="^"
+OPTION_KEY_SYMBOL="~"
+SHIFT_KEY_SYMBOL="$"
+TAB_KEY_SYMBOL="\\U21e5" # Note the backslash-escaped backslash.
+
+# # You may need to restart the Finder or kill the preferences daemon for the change to take effect:
+# killall Finder
+# killall cfprefsd
+
+DESTFILE=install-hotkeys.sh
+echo '#!/bin/bash' > $DESTFILE
+
+defaults find NSUserKeyEquivalents | sed -e "s/Found [0-9]* keys in domain '\\([^']*\\)':/defaults write \\1 NSUserKeyEquivalents '/" -e "s/    NSUserKeyEquivalents =     {//"  -e "s/};//" -e "s/}/}'/" >> $DESTFILE
+echo killall Finder >> $DESTFILE
+echo killall cfprefsd >> $DESTFILE
+chmod a+x $DESTFILE
