3d75c3ba949f93e137b8c5d64bd0dc56c7c1c363
Aaron Culich <aculich@gmail.com>
2018-09-05 18:00:35 -0700

    gracefully handle git/pgp/magit integration

--- user.sh
+++ user.sh
@@ -17,3 +17,6 @@
 git config --global user.name "Aaron Culich"
 git config --global user.email "aculich@gmail.com"
 git config --global credential.helper osxkeychain
+# https://gist.github.com/aculich/01c38995ac7c6610a054f37ebdbe30b8
+# echo pinentry-program /usr/local/MacGPG2/libexec/pinentry-mac.app/Contents/MacOS/pinentry-mac >> ~/.gnupg/gpg-agent.conf
+# gpgconf --kill gpg-agent
